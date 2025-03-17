b2b4c531d9481430320876db307f32d441b5d017
KalimsCodingWonderland <159584563+KalimsCodingWonderland@users.noreply.github.com>
2025-03-17 11:08:30 -0400

    Changed back to locally hosted DB.

--- constants/api.ts
+++ constants/api.ts
@@ -1,6 +1,6 @@
 //constants/api.ts
 
-const API_BASE =  'https://align-git-kalimbranch-kalims-projects-52343af1.vercel.app/';
+const API_BASE =  'http://192.168.1.147:5001';
 
 
 export const registerUser = async (username: string, email: string, password: string) => {
